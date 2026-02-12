37afbb866e3bebe7bd254dc053a28d3c3ca1b83d
hrishikesh-kamble-1397 <hrishikeshkamble131297@gmail.com>
2026-02-12 17:00:10 +0530

    Update demo_streamlit_app.py

--- demo_streamlit_app.py
+++ demo_streamlit_app.py
@@ -43,7 +43,7 @@ run_proc = st.sidebar.button("Run PII/PHI Parse & Classify")
 # -----------------------------------------------------------------------------
 # Helpers
 # -----------------------------------------------------------------------------
-def call_search(query: str, k: int, serivce: str) -> pd.DataFrame:
+def call_search(query: str, k: int, service: str) -> pd.DataFrame:
     search_sql = """
        SELECT
           CHUNK_TEXT,
