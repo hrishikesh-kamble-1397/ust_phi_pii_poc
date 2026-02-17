5ead3957bb1a570d87e91acf1f249da03522ac54
hrishikesh-kamble-1397 <hrishikeshkamble131297@gmail.com>
2026-02-17 16:01:46 +0530

    Update demo_streamlit_app.py

--- demo_streamlit_app.py
+++ demo_streamlit_app.py
@@ -145,7 +145,7 @@ def call_search(query: str, k: int) -> pd.DataFrame:
             c.CHUNK_TEXT,
             c.SOURCE_FILE,
             VECTOR_COSINE_SIMILARITY(c.EMBEDDING, q.emb) AS SCORE
-        FROM PDF_CHUNKS c
+        FROM DOCS_CHUNKS c
         CROSS JOIN query_vec q
         ORDER BY SCORE DESC
         LIMIT {k}
