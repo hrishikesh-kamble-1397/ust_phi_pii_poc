14fbddfeda7616e6219fa2cc524418a25ac1a759
hrishikesh-kamble-1397 <hrishikeshkamble131297@gmail.com>
2026-02-23 18:24:17 +0530

    Update demo_streamlit_app.py

--- demo_streamlit_app.py
+++ demo_streamlit_app.py
@@ -153,7 +153,7 @@ def call_search(query):
                 c.CLEAN_TEXT,
                 c.SOURCE_FILE,
                 VECTOR_COSINE_SIMILARITY(c.EMBEDDING, q.emb) AS SCORE
-            FROM AI_POC_DB.PII_PHI_POC.CLEANED_CHUNKS c
+            FROM AI_POC_DB.PII_PHI_POC.CLEANED_CHUNK_TEXT c
             CROSS JOIN query_vec q
         )
         WHERE SCORE >= {SIMILARITY_THRESHOLD}
