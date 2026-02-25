427804c00619fc7fdc7531646584fecbfd45c0ab
parwesandeep <sandeep.s.parwe@gmail.com>
2026-02-25 18:26:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from snowflake.snowpark.context import get_active_session
 # Streamlit Config
 # -----------------------------------------------------------------------------
 st.set_page_config(page_title="AI Database Chatbot", layout="wide")
-st.title("🤖 AI Database Chatbot (Auto Schema Discovery)")
+st.title("🤖 AI Database Discovery)")
 
 session = get_active_session()
 
